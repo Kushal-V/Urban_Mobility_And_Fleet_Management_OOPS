35f2f42356b208ea58ed3ecba18dc647cfd408fc
Kushal <kushalvekariya98@gmail.com>
2026-01-03 15:20:21 +0530

    setup : [Kushal] created abstract class

--- vehicle.py
+++ vehicle.py
@@ -1,4 +1,6 @@
-class Vehicle:
+from abc import ABC, abstractmethod
+
+class Vehicle(ABC):
     def __init__(self, vehicle_id, model, battery_percentage):
         self.vehicle_id = vehicle_id
         self.model = model
@@ -40,3 +42,7 @@ class Vehicle:
             self.__status = status
         else:
             print("Invalid status")
+
+    @abstractmethod
+    def calculate_trip_cost(self, distance):
+        pass
